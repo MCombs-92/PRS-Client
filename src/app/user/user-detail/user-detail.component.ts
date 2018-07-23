509ffcf3bdab0d10406e9c7b41b84217ff9836fd
Mitchell Combs <mitch.combs.92@gmail.com>
2018-07-23 14:02:10 -0400

    Added functionality for User detail, Edit and remove

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  boolOptions = [
+    { display: 'Yes', value: true },
+    { display: 'No', value: false }
+  ];
+
+  Removed: boolean = false;
+
+  user: User = new User;
+
+  Remove(): void {
+    this.Removed = true;
+  }
+
+  Verified(): void {
+    this.Removed = false;
+    this.usersvc.remove(this.user)
+      .subscribe(resp => {
+        console.log("Remove user:", resp);
+        this.router.navigateByUrl('/users/list');
+      });
+  }
+
+  constructor(
+    private usersvc: UserService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+    this.usersvc.get(id).subscribe(resp => {
+      console.log(resp)
+      this.user = resp.Data;
+    }) 
   }
 
 }
--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  boolOptions = [
+    { display: 'Yes', value: true },
+    { display: 'No', value: false }
+  ];
+
+  user: User = new User;
+
+  change(): void {
+    this.usersvc.change(this.user).subscribe(resp => {
+      console.log(resp);
+      this.router.navigateByUrl("/users/list");
+    })
+  }
+
+  constructor(
+    private usersvc: UserService, 
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id;
+    this.usersvc.get(id)
+      .subscribe(resp => {
+        this.user = resp.Data;
+      });
   }
 
 }
